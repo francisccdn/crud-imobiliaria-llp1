a4720601ebdc949c0b074a4e71736d672c4e2b7d
Joelder-Victor <joelder.antonino@gmail.com>
2019-10-31 20:45:51 -0300

    usuario.c Se cadastrem, lembrem de mudar o indice do array usuarios

---
+++ usuario.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "registro.h"
+#include "funcoes.h"
+#include "usuario.h"
+
+#define MAX_TAMANHO 100
+
+
+
+imovel_t listaImoveis[MAX_TAMANHO];
+
+usuario_t usuarios[MAX_TAMANHO];
+
+int main(void)
+{
+	puts("entrei");
+
+	usuarios[0].ultimo=1;
+
+	lerArquivoUsuarios();
+
+	//cadastraUsuario();
+
+	exibeUsuario(0);
+
+	//salvaAquivoUsuarios();
+
+	return(0);
+
+}
