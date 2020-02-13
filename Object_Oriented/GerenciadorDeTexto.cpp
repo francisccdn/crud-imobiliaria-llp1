08bd05d128e8edac0d05e659e1c93ccc30f3947c
Francisco Cunha <francisccdn@gmail.com>
2020-02-13 11:08:34 -0300

    Remove acento

--- Object_Oriented/GerenciadorDeTexto.cpp
+++ Object_Oriented/GerenciadorDeTexto.cpp
@@ -1,21 +1,38 @@
 #include "GerenciadorDeTexto.h"
-#include <iostream>
 
-std::string GerenciadorDeTexto::strToLower(std::string str){
-    for(int i = 0; i < str.length(); i++){
+std::string GerenciadorDeTexto::strToLower(std::string str)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
         str[i] = tolower(str[i]);
     }
     return str;
 }
 
-//A fazer
-std::string GerenciadorDeTexto::removeAcento(std::string str){
-    std::string comAcentos = "ÄÅÁÂÀÃäáâàãÉÊËÈéêëèÍÎÏÌíîïìÖÓÔÒÕöóôòõÜÚÛüúûùÇç";
-    std::string semAcentos = "AAAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUuuuuCc";
-    
-    for (int i = 0; i < comAcentos.length(); i++)
+std::string GerenciadorDeTexto::removeAcento(std::string str)
+{   /*
+    Para entender essa função, é preciso saber que para processar uma letra com acento c++ usa dois chars:
+    um para indicar que o proximo char é um acento, e logo depois o acento em si.
+    Por isso comAcentos.size() == 2*(semAcentos.size() - 1) + 1;
+    */
+    std::string comAcentos = "äáâàãéêëèíîïìöóôòõüúûùç";
+    std::string semAcentos = "aaaaaeeeeiiiiooooouuuuc";
+    char acento;
+
+    for (int i = 0; i < str.length(); i++)
     {
-       // str = str.replace(comAcentos[i].ToString(), semAcentos[i].ToString());
+        if(str[i] == comAcentos[0])
+        {
+            acento = str[i+1];
+            for(int j = 0; j < comAcentos.length(); j++)
+            {
+                if(comAcentos[j] == acento)
+                {
+                    str[i] = semAcentos[j/2];
+                    str.erase(str.begin() + i+1);
+                }
+            }
+        }
     }
     return str;
 }
